89e98065b50cb984ecd57143879d823b97bf6b18
noltecasey <noltecasey9@gmail.com>
2025-05-08 00:08:05 +0200

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './assets/styles/App.css'
+import Footer from "./views/Footer.jsx";
 
 function App() {
 
   return (
+      <div className="App">
     <>
       <h1>Edu Transit Web Client!</h1>
     </>
-  )
+          <Footer/>
+      </div>
+  );
 }
 
 export default App
