14e2f30642003e7a4cda6297db414c62df2d6e0d
EthanLeRoux <127761539+EthanLeRoux@users.noreply.github.com>
2025-05-09 18:31:58 +0200

    Add React Router and restructure components
    Integrated `react-router-dom` for routing and updated dependencies. Refactored components and views, including the introduction of `MainSection` and updates to `HomePage` for better modularity. Wrapped the app in `Router` and defined routes for improved navigation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './assets/styles/App.css';
 import Navigation from './components/Navigation';
+import MainSection from './components/MainSection.jsx';
+import Footer from "./components/Footer.jsx";
+import HomePage from "./views/HomePage.jsx";
 
 function App() {
     return (
         <>
-            <Navigation />
-            <Main />
-            <Footer />
+            <Router>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </Router>
         </>
     );
 }
