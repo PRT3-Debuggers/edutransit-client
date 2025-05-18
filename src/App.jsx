5cc11ed3ac2a23c17025b6814d43d6576389bd7a
Casey Abigail Nolte <102228884+Casey-Nolte@users.noreply.github.com>
2025-05-18 19:29:23 +0200

    Merge pull request #8 from PRT3-Debuggers/Casey
    Casey

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './assets/styles/App.css';
 import Navigation from './components/Navigation';
 import MainSection from './components/MainSection.jsx';
 import Footer from "./components/Footer.jsx";
 import HomePage from "./views/HomePage.jsx";
 
+import Footer from './components/Footer';
+import About from './pages/About';
+import Drivers from './pages/Drivers';
+import PrivacyPolicy from './pages/PrivacyPolicy';
+
+
 function App() {
     return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                </Routes>
-            </Router>
-        </>
+        <BrowserRouter>
+        <div className="App">
+            <Navigation />
+            <Main>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<About/>}/>
+                <Route path="/drivers" element={<Drivers/>}/>
+                <Route path"/privacy" element={<PrivacyPolicy/>}/>
+            </Routes>
+            </ Main>
+            <Footer />
+            </div>
+        </BrowserRouter>
     );
 }
 
